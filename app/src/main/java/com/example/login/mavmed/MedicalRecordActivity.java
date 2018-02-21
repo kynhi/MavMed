42db625d87d4f0222587e4e432421820aee2f965
Francis <lefrancis12@yahoo.com>
2018-02-20 18:24:40 -0600

    testing add button
    with hard code values

--- app/src/main/java/com/example/login/mavmed/MedicalRecordActivity.java
+++ app/src/main/java/com/example/login/mavmed/MedicalRecordActivity.java
@@ -1,12 +1,18 @@
 package com.example.login.mavmed;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+
 
 public class MedicalRecordActivity extends AppCompatActivity {
 
@@ -14,39 +20,49 @@ public class MedicalRecordActivity extends AppCompatActivity {
     private ExpandableListAdapter listAdapter;
     private List<String> listDataHeader;
     private HashMap<String,List<String>> listHash;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medical_record);
-
         listView = (ExpandableListView)findViewById(R.id.lvExp);
-        initData();
-        listAdapter = new ExpandableListAdapter(this,listDataHeader,listHash);
-        listView.setAdapter(listAdapter);
-    }
-
-    private void initData() {
         listDataHeader = new ArrayList<>();
         listHash = new HashMap<>();
 
         listDataHeader.add("Allergies");
         listDataHeader.add("Immunization");
         listDataHeader.add("Medication");
-
-
         List<String> allergies = new ArrayList<>();
-        allergies.add("Peanut");
-        allergies.add("Siracha");
-
         List<String> immune = new ArrayList<>();
-        immune.add("Cheese");
-        immune.add("Bugs");
-
-        List<String> med = new ArrayList<>();
-        med.add("icecream");
-
+        final List<String> med = new ArrayList<>();
         listHash.put(listDataHeader.get(0),allergies);
         listHash.put(listDataHeader.get(1),immune);
         listHash.put(listDataHeader.get(2),med);
+
+        allergies.add("Peanut");
+        allergies.add("Siracha");
+        immune.add("Cheese");
+        immune.add("Bugs");
+        med.add("icecream");
+        med.add("cookies");
+
+
+        listAdapter = new ExpandableListAdapter(this,listDataHeader,listHash);
+        listView.setAdapter(listAdapter);
+        final Button button = findViewById(R.id.button_addMR);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                med.add("LOVE");
+                Context context = getApplicationContext();
+                CharSequence text = "LOOK AT MEDICINE!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
+
 }
