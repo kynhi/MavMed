c9ba7fa24cefeb772ae023a1c9a687fcf6008979
Francis <lefrancis12@yahoo.com>
2018-02-23 01:53:56 -0600

    Adding medical record with radio button and edittext. Need work for "cancel dialog"

--- app/src/main/java/com/example/login/mavmed/MedicalRecordActivity.java
+++ app/src/main/java/com/example/login/mavmed/MedicalRecordActivity.java
@@ -1,11 +1,13 @@
 package com.example.login.mavmed;
 
-import android.content.Context;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ExpandableListView;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -21,11 +23,13 @@ public class MedicalRecordActivity extends AppCompatActivity {
     private List<String> listDataHeader;
     private HashMap<String,List<String>> listHash;
 
-    @Override
+    int category;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medical_record);
+
         listView = (ExpandableListView)findViewById(R.id.lvExp);
         listDataHeader = new ArrayList<>();
         listHash = new HashMap<>();
@@ -33,8 +37,8 @@ public class MedicalRecordActivity extends AppCompatActivity {
         listDataHeader.add("Allergies");
         listDataHeader.add("Immunization");
         listDataHeader.add("Medication");
-        List<String> allergies = new ArrayList<>();
-        List<String> immune = new ArrayList<>();
+        final List<String> allergies = new ArrayList<>();
+        final List<String> immune = new ArrayList<>();
         final List<String> med = new ArrayList<>();
         listHash.put(listDataHeader.get(0),allergies);
         listHash.put(listDataHeader.get(1),immune);
@@ -50,19 +54,69 @@ public class MedicalRecordActivity extends AppCompatActivity {
 
         listAdapter = new ExpandableListAdapter(this,listDataHeader,listHash);
         listView.setAdapter(listAdapter);
-        final Button button = findViewById(R.id.button_addMR);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-                med.add("LOVE");
-                Context context = getApplicationContext();
-                CharSequence text = "LOOK AT MEDICINE!";
-                int duration = Toast.LENGTH_SHORT;
 
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
+        final Button mShowDialog = (Button) findViewById(R.id.button_addMR);
+        mShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MedicalRecordActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.add_medicalrecord, null);
+                final EditText inputname = (EditText) mView.findViewById(R.id.et_MRtext);
+                final Button addconfirm = (Button) mView.findViewById(R.id.button_addMR_dialog);
+                final Button cancel = (Button) mView.findViewById(R.id.button_cancel_MR_dialog);
+//adding all the input and process
+                addconfirm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        if (!inputname.getText().toString().isEmpty() && category == 0) {
+                            Toast.makeText(MedicalRecordActivity.this,
+                                    "New Medical Record Added!", Toast.LENGTH_LONG).show();
+                            allergies.add(inputname.getText().toString());
+                        } else if (!inputname.getText().toString().isEmpty() && category == 1) {
+                            Toast.makeText(MedicalRecordActivity.this,
+                                    "New Medical Record Added!", Toast.LENGTH_LONG).show();
+                            immune.add(inputname.getText().toString());
+                        } else if (!inputname.getText().toString().isEmpty() && category == 2) {
+                            Toast.makeText(MedicalRecordActivity.this,
+                                    "New Medical Record Added!", Toast.LENGTH_LONG).show();
+                            med.add(inputname.getText().toString());
+                        } else {
+                            Toast.makeText(MedicalRecordActivity.this,
+                                    "Please Fill in Empty Field", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
+                mBuilder.setView(mView);
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
             }
         });
-    }
 
+    }
+    public void checkButton(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch(view.getId()) {
+            case R.id.radio_allergies:
+                if (checked)
+                    category = 0;
+                    break;
+            case R.id.radio_immune:
+                if (checked)
+                    category = 1;
+                break;
+            case R.id.radio_med:
+                if (checked)
+                    category = 2;
+                break;
+        }
+    }
 }
