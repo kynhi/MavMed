75a196f154da94742506e2f95bc815b9ea5d3dd5
Nhi Luong <nhi.luong@mavs.uta.edu>
2018-03-04 15:11:30 -0600

    Navigation has homepage and Diagnosis search works

--- app/src/main/java/com/example/login/mavmed/activity/HomeFragment.java
+++ app/src/main/java/com/example/login/mavmed/activity/HomeFragment.java
@@ -5,17 +5,32 @@ package com.example.login.mavmed.activity;
  */
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.login.mavmed.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class HomeFragment extends Fragment {
+    Button logout ;
+    Button diagnose;
+    Button medicalRecord;
 
+    private FragmentDrawer drawerFragment; //Fragment
+    // Creating FirebaseAuth.
+    FirebaseAuth firebaseAuth ;
+    // Creating FirebaseUser.
+    FirebaseUser firebaseUser;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -31,7 +46,59 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
+        diagnose = (Button) rootView.findViewById(R.id.diag_search);
+        logout = (Button) rootView.findViewById(R.id.Logout);
+        medicalRecord = (Button) rootView.findViewById(R.id.MedicalRecord);
+        diagnose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), DiagnosisSearchFragment.class);
+                startActivity(intent);
+            }
+        });
+        medicalRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MedicalRecordActivity.class);
+                startActivity(intent);
+            }
+        });
+        firebaseAuth =  FirebaseAuth.getInstance();
+        // On activity start check whether there is user previously logged in or not.
+        if(firebaseAuth.getCurrentUser() == null){
 
+            // Finishing current Profile activity.
+            getActivity().finish();
+
+            // If user already not log in then Redirect to LoginActivity .
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+
+            // Showing toast message.
+            Toast.makeText(getActivity(), "Please Log in to continue", Toast.LENGTH_LONG).show();
+
+        }
+        // Adding firebaseAuth current user info into firebaseUser object.
+        firebaseUser = firebaseAuth.getCurrentUser();
+        // Adding click listener on logout button.
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("MAYMAV","CHOOSE DRAWER");
+                // Destroying login season.
+                firebaseAuth.signOut();
+
+                // Finishing current User Profile activity.
+                getActivity().finish();
+
+                // Redirect to Login Activity after click on logout button.
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+
+                // Showing toast message on logout.
+                Toast.makeText(getActivity(), "Logged Out Successfully.", Toast.LENGTH_LONG).show();
+            }
+        });
         // Inflate the layout for this fragment
         return rootView;
     }
