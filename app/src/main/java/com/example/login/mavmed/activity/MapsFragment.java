1c4f36388a2bbe9195ceb8ba0ce9ff182b442cef
bobbykemp <robert.kemp@mavs.uta.edu>
2018-04-08 22:03:52 -0500

    added time picker for daily med reminder, user calendar input

--- app/src/main/java/com/example/login/mavmed/activity/MapsFragment.java
+++ app/src/main/java/com/example/login/mavmed/activity/MapsFragment.java
@@ -30,7 +30,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
