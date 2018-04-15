4854fe5bae2a1f83566e91aa0888927334489874
Nhi Luong <nhi.luong@mavs.uta.edu>
2018-04-15 14:47:45 -0500

    buttons on nav bar and home page works

--- app/src/main/java/com/example/login/mavmed/activity/LoginActivity.java
+++ app/src/main/java/com/example/login/mavmed/activity/LoginActivity.java
@@ -25,7 +25,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends Activity {
 
-    Button login,register,skip;
+    Button login,register,skip,forgetpassword;
     EditText email,password;
 
 
@@ -51,6 +51,7 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
 
         login = (Button)findViewById(R.id.Login);
+        forgetpassword = (Button) findViewById(R.id.Forget_password);
         email = (EditText)findViewById(R.id.Email);
         password = (EditText)findViewById(R.id.Password);
 
@@ -84,47 +85,6 @@ public class LoginActivity extends Activity {
 
                 if (EditTextEmptyHolder) {
 
-/*
-                    LoginDatabase = LoginDbHelper.getWritableDatabase();
-
-                    // Adding search username query to cursor.
-                    //cursor = LoginDatabase.query(LoginContract.LoginEntry.TABLE_NAME, null, " " +
-                    //        LoginContract.LoginEntry.COLUMN_USERNAME + "=?", new String[]{emailHolder}, null, null, null);
-                    while (cursor.moveToNext()) {
-
-                        if (cursor.isFirst()) {
-
-                            cursor.moveToFirst();
-
-                            // Storing Password associated with entered email.
-                            TempPassword = cursor.getString(cursor.getColumnIndex(LoginContract.LoginEntry.COLUMN_PASSWORD));
-
-                            // Closing cursor.
-                            cursor.close();
-                        }
-                    }
-                    if(TempPassword.equalsIgnoreCase(passwordHolder))
-                    {
-
-                        Toast.makeText(LoginActivity.this,"Login Successfully",Toast.LENGTH_LONG).show();
-
-                        // Going to Dashboard activity after login success message.
-                        Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
-                        startActivity(intent);
-                    }
-
-                    else {
-                        Toast.makeText(LoginActivity.this,"UserName or Password is Wrong, Please Try Again.",Toast.LENGTH_LONG).show();
-                        attempts_count.setVisibility(View.VISIBLE);
-                        attempts_count.setBackgroundColor(Color.RED);
-                        counter--;
-                        attempts_count.setText(Integer.toString(counter));
-
-                        if (counter == 0) {
-                            login.setEnabled(false);
-                        }
-                    }
-                    TempPassword = "NOT_FOUND" ;*/
                     LoginFunction();
                 } else {
                     Toast.makeText(LoginActivity.this, "Please Enter UserName or Password.", Toast.LENGTH_LONG).show();
@@ -133,6 +93,16 @@ public class LoginActivity extends Activity {
             }
         );
 
+        //Forget Password Button
+        forgetpassword.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                // Going to Reset Password Activity
+                Intent intent = new Intent(LoginActivity.this, ResetPasswordActivity.class);
+                startActivity(intent);
+            }
+        } );
         skip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
